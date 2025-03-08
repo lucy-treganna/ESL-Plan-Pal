e5642e059b9332719e3e4ad2f0ab37d6caaafee5
Lucy Treganna <lucy.treganna@gmail.com>
2025-03-08 12:52:14 +0100

    started building form

--- app/_components/Form.js
+++ app/_components/Form.js
@@ -1,13 +1,78 @@
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
+import FormButton from "@/app/_components/FormButton";
+
 export default function Form() {
   return (
-    <form className="bg-white">
-      <div className="space-y-12">
-        <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base/7 font-semibold text-gray-900">Profile</h2>
-          <p className="mt-1 text-sm/6 text-gray-600">
-            This information will be displayed publicly so be careful what you
-            share.
+    <form>
+      <div className="bg-cool-white rounded-xl shadow-sm space-y-12 max-w-3xl mx-auto">
+        <div className="py-6 px-8">
+          <h2 className="text-base/7 font-bold text-grey-purple">
+            Quick Generate
+          </h2>
+          <p className="mt-1 text-sm/6 text-charcoal/80">
+            Try our quick generate feature when you&apos;re short on time. Enter
+            your criteria below and get ESL resources for your lesson.
           </p>
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label htmlFor="age" className="block text-sm/6 font-medium">
+                How old are your students?
+              </label>
+              <div className="mt-2 grid grid-cols-1">
+                <select
+                  id="age"
+                  name="age"
+                  className="col-start-1 row-start-1 w-full appearance-none rounded-md py-1.5 pr-8 pl-3 text-base outline-1 -outline-offset-1 outline-charcoal/20 focus:outline-2 focus:-outline-offset-2 focus:outline-medium-blue sm:text-sm/6"
+                >
+                  <option>Under 3 years</option>
+                  <option>4-5 years</option>
+                  <option>5-6 years</option>
+                </select>
+                <ChevronDownIcon
+                  aria-hidden="true"
+                  className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end sm:size-4"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label htmlFor="level" className="block text-sm/6 font-medium">
+                What is their English level?
+              </label>
+              <div className="mt-2 grid grid-cols-1">
+                <select
+                  id="level"
+                  name="level"
+                  className="col-start-1 row-start-1 w-full appearance-none rounded-md py-1.5 pr-8 pl-3 text-base outline-1 -outline-offset-1 outline-charcoal/20 focus:outline-2 focus:-outline-offset-2 focus:outline-medium-blue sm:text-sm/6"
+                >
+                  <option>Beginner</option>
+                  <option>Intermediate</option>
+                  <option>Advanced</option>
+                </select>
+                <ChevronDownIcon
+                  aria-hidden="true"
+                  className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end sm:size-4"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label htmlFor="numSongs" className="block text-sm/6 font-medium">
+                How many songs would you like to include?
+              </label>
+              <div className="mt-2">
+                <input
+                  id="numSongs"
+                  name="numSongs"
+                  type="number"
+                  className="block w-full rounded-md px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-charcoal/20 focus:outline-2 focus:-outline-offset-2 focus:outline-medium-blue sm:text-sm/6"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="pt-6">
+            <FormButton text="Generate resources" />
+          </div>
         </div>
       </div>
     </form>
--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import { Inter, Merriweather_Sans, Quicksand, Poppins } from "next/font/google";
+import { Inter, Merriweather_Sans, Poppins } from "next/font/google";
 import "./globals.css";
 
 import NavBar from "@/app/_components/NavBar";
@@ -15,11 +15,6 @@ const merriweather = Merriweather_Sans({
   display: "swap",
 });
 
-const quicksand = Quicksand({
-  subsets: ["latin"],
-  variable: "--font-quicksand",
-  display: "swap",
-});
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -38,7 +33,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${merriweather.variable} ${inter.variable} ${quicksand.variable} ${poppins.variable} antialiased bg-pale-blue min-h-screen relative overflow-hidden`}
+        className={`${merriweather.variable} ${inter.variable} ${poppins.variable} antialiased bg-pale-blue text-charcoal min-h-screen relative overflow-hidden`}
       >
         <NavBar />
         <div className="flex-1 px-8 py-12 grid">
